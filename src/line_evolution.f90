dd56f43e82581943793de0ed737c606c887af377
p-hss <philipp-hess@gmx.de>
2019-02-19 17:32:30 +0100

    up

--- src/line_evolution.f90
+++ src/line_evolution.f90
@@ -99,8 +99,8 @@ subroutine growth_rates(frame)
 
         line_element_dir(:) = matmul(strain_rate(:,:), le_local(lpi,2,:,1)/le_length_local(lpi,2,1))
 
-        zeta(lpi) = dot_product(line_element_dir(:), le_local(lpi,2,:,1)/le_length_local(lpi,2,1))
-        !zeta(lpi) = rand(int(lpi**3,4))
+        !zeta(lpi) = dot_product(line_element_dir(:), le_local(lpi,2,:,1)/le_length_local(lpi,2,1))
+        zeta(lpi) = rand(int(lpi**3,4))
 
         !============method #2===================
         !zeta(lpi) = (log(le_length_local(lpi,2,1)) - log(le_length_local(lpi,1,1)))/dt 
